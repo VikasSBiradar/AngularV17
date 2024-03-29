a103fa8d7501e7218c1154b6355d610f6a151bfb
VikasBiradar <vikasbiradar0802@gmail.com>
2024-03-29 10:03:12 +0530

    ECMA

---
+++ TYPESCRIPT/06-Classes/p1.js
@@ -0,0 +1,16 @@
+//ES-5
+function Person(ssn, firstName, lastName) {
+    var x, y;
+    this.ssn1 = ssn;
+    this.firstName1 = firstName;
+    this.lastName1 = lastName;
+}
+Person.prototype.getFullName = function () {
+    return this.firstName1 + " " + this.lastName1;
+};
+Person.prototype.age = 100;
+var person = new Person('R17CS527', 'John', 'Doe');
+console.log("PERSON=", person);
+console.log("PERSON.SSN1=", person.ssn1);
+console.log("person.Getfullname=", person.getFullName());
+console.log("person.age=", person.age);

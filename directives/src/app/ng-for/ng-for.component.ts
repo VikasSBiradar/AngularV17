5d46edf4f46d538529906d6e18c41d01a9056189
VikasBiradar <vikasbiradar2016@gmail.com>
2024-04-07 13:03:28 +0530

    ngfor

--- directives/src/app/ng-for/ng-for.component.ts
+++ directives/src/app/ng-for/ng-for.component.ts
@@ -1,12 +1,45 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-ng-for',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './ng-for.component.html',
   styleUrl: './ng-for.component.css'
 })
 export class NgForComponent {
+  empName : any = ['A','AB','ABC','ABC','ABCDE',100,200]
 
+  animals : any =['Tiger','Lion','Elephant'];
+
+  employee : any = [
+    {
+      name : "HTML",
+      email : 'html@gmail.com'
+    },
+    {
+      name : "CSS",
+      email : 'css@gmail.com'
+    },
+    {
+      name : "JS",
+      email : 'js@gmail.com'
+    }
+  ];
+
+
+  addEmployee(name : string, email:string){
+    console.log(name);
+    console.log(email);
+    this.employee.push({
+      name : name,
+      email : email
+    });
+  }
+
+  removeEmployee(i : number){
+    alert(`Removed employee with index ${i}`);
+    this.employee.splice(i,1);
+  }
 }

b2bc6ed7d1a029e94e4509f39e8cc704c33f0a71
VikasBiradar <vikasbiradar2016@gmail.com>
2024-04-27 12:35:03 +0530

    Static form changes

--- Forms/src/app/components/static-forms/static-forms.component.ts
+++ Forms/src/app/components/static-forms/static-forms.component.ts
@@ -1,12 +1,21 @@
+import { JsonPipe, NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { User } from '../../class/user';
 
 @Component({
   selector: 'app-static-forms',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,NgFor,JsonPipe, NgIf],
   templateUrl: './static-forms.component.html',
   styleUrl: './static-forms.component.css'
 })
 export class StaticFormsComponent {
+  topics: any = ["Angular", "React", "Node", "Javacript"];
 
+  userFormData(formData : any){
+    console.log(`Form data = ${JSON.stringify(formData)}`);
+  }
+
+  modelForm = new User("Rust Marty","rustmarty@gmail.com","8142381423","React","morning","subscribed")
 }

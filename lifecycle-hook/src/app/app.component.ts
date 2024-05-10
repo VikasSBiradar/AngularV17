d6b8c7b00a3ad571cceb82509f48703e3b2b9d8b
VikasBiradar <vikasbiradar2016@gmail.com>
2024-05-10 10:02:11 +0530

    lifecycle-hook

---
+++ lifecycle-hook/src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { ChildComponent } from './child/child.component';
+import { NgIf } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet,ChildComponent, NgIf],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  constructor(){
+    console.log(`App component called`);
+  }
+
+  inputVal : string = "";
+  toDestroy : boolean = false;
+
+  onBtnClicked(inputEle : HTMLInputElement){
+    console.log(`inputEle=${inputEle}`);
+
+    this.inputVal = inputEle.value;
+
+    console.log(`inputVal=${this.inputVal}`);
+
+  }
+}
